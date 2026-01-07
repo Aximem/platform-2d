d3b383cd5a006a9598fd53234acf650758c1838d
Maxime CHARRUEL <m.charruel@gmail.com>
2026-01-07 07:47:16 +0100

    Fix release inputs on death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -9,6 +9,14 @@ func _on_body_entered(body: Node2D) -> void:
 		var playerCollisionShape2D: CollisionShape2D = get_tree().current_scene.get_node("Player/CollisionShape2D")
 		playerCollisionShape2D.set_deferred("disabled", true)
 		Engine.time_scale = 0.5
+		
+		# Release inputs
+		Input.action_release("move_left")
+		Input.action_release("move_right")
+		Input.action_release("climb")
+		Input.action_release("descend")
+		Input.action_release("jump")
+		
 		timer.start()
 
 func _on_timer_timeout() -> void:
