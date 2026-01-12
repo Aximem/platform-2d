666de6bf65557d40d3ed1d2c06d69527c78f88ce
Maxime CHARRUEL <m.charruel@gmail.com>
2026-01-12 08:35:19 +0100

    Add scene bomb

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var tile_map_layer_bridge: TileMapLayer = $TileMaps/TileMapLayerBridge
+@onready var tile_map_layer_moving_platform: TileMapLayer = $TileMaps/TileMapLayerMovingPlatform
 
 func _ready() -> void:
 	GameManager.validate_enigma.connect(_on_validate_enigma)
