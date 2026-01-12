ac5cee1c944bfa94e2d9f87ddec5de12878b3ef4
Maxime CHARRUEL <m.charruel@gmail.com>
2026-01-12 08:44:47 +0100

    Add moving platform

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,12 +3,36 @@ extends Node2D
 @onready var tile_map_layer_bridge: TileMapLayer = $TileMaps/TileMapLayerBridge
 @onready var tile_map_layer_moving_platform: TileMapLayer = $TileMaps/TileMapLayerMovingPlatform
 
+var platform_speed: float = 75.0
+var platform_direction: int = 1  # 1 = droite, -1 = gauche
+var platform_initial_position: Vector2
+var platform_max_distance: float = 125.0
+
 func _ready() -> void:
 	GameManager.validate_enigma.connect(_on_validate_enigma)
-	
+
 	tile_map_layer_bridge.visible = false
 	tile_map_layer_bridge.collision_enabled = false
-	
-func _on_validate_enigma(pnjId: int):
+
+	platform_initial_position = tile_map_layer_moving_platform.position
+
+func _process(delta: float) -> void:
+	_move_platform(delta)
+
+func _move_platform(delta: float) -> void:
+	tile_map_layer_moving_platform.position.x += platform_speed * platform_direction * delta
+
+	var current_offset = tile_map_layer_moving_platform.position.x - platform_initial_position.x
+
+	# Atteint la limite droite (500px)
+	if current_offset >= platform_max_distance:
+		tile_map_layer_moving_platform.position.x = platform_initial_position.x + platform_max_distance
+		platform_direction = -1
+	# Revenu à la position initiale
+	elif current_offset <= 0:
+		tile_map_layer_moving_platform.position.x = platform_initial_position.x
+		platform_direction = 1
+
+func _on_validate_enigma(_pnjId: int):
 	tile_map_layer_bridge.visible = true
 	tile_map_layer_bridge.collision_enabled = true
